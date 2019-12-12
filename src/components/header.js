41674e8a0fb26a410e40474099ce71e5956a51dd
malan88 <malan88@standingwater.io>
2019-12-12 13:03:00 -0500

    Successful snap nav
    Still needs to be styled, still needs a burger button. The documentation
    is more bare bones than I thought. Kind of irritating.

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,10 @@
 import { useStaticQuery, graphql, Link } from "gatsby"
 import React from "react"
 import styled from "styled-components"
+import { slide as Menu } from "react-burger-menu"
 import logo from "../images/logo.png"
+import Dimensions from "../dimensions"
+import { nums } from "../breakpoints"
 
 const Container = styled.header`
     background-color: white;
@@ -13,20 +16,6 @@ const Container = styled.header`
     z-index: 1;
     width: 100%;
 `
-const Nav = styled.nav`
-    a {
-        margin-left: 0.5rem;
-
-        text-decoration: none;
-        text-transform: uppercase;
-        color: black;
-        font-family: Roboto;
-        font-weight: 700;
-
-        transition: color 0.3s ease;
-        &:hover { color: red; }
-    }
-`
 const Title = styled.h1`
     font-family: Roboto;
     text-transform: uppercase;
@@ -34,16 +23,61 @@ const Title = styled.h1`
     a {
         text-decoration: none;
         color: black;
-        margin: auto 0.25rem;
         transition: color 0.3s ease;
         &:hover { color: red; }
     }
 `
 
+const NavDesktop = () => {
+    const Nav = styled.nav`
+        a {
+            margin-left: 0.5rem;
+
+            text-decoration: none;
+            text-transform: uppercase;
+            color: black;
+            font-family: Roboto;
+            font-weight: 700;
+
+            transition: color 0.3s ease;
+            &:hover { color: red; }
+        }
+    `
+    return (
+        <Nav>
+            <Link to="/about" activeClassName="active">About</Link>
+            <Link to="/projects" activeClassName="active">Projects</Link>
+            <Link to="/services" activeClassName="active">Services</Link>
+            <Link to="/contact" activeClassName="active">Contact</Link>
+        </Nav>
+    )
+}
+
+class NavMobile extends React.Component {
+    showSettings (event) {
+        event.preventDefault();
+    }
+    render () {
+        return (
+            <Menu>
+                <Link to="/about" activeClassName="active">About</Link>
+                <Link to="/projects" activeClassName="active">Projects</Link>
+                <Link to="/services" activeClassName="active">Services</Link>
+                <Link to="/contact" activeClassName="active">Contact</Link>
+            </Menu>
+        );
+    }
+}
+
+
 const Header = () => {
     const data = useStaticQuery(
         graphql`query { site { siteMetadata { title } } }`
     )
+    const { height, width } = Dimensions();
+
+    const Nav = width < nums.vp4 ? NavMobile : NavDesktop
+
     return (
         <Container>
             <Title>
@@ -52,12 +86,7 @@ const Header = () => {
                 </Link>
                 <Link to="/">{data.site.siteMetadata.title}</Link>
             </Title>
-            <Nav>
-                <Link to="/about" activeClassName="active">About</Link>
-                <Link to="/projects" activeClassName="active">Projects</Link>
-                <Link to="/services" activeClassName="active">Services</Link>
-                <Link to="/contact" activeClassName="active">Contact</Link>
-            </Nav>
+            <Nav />
         </Container>
     )
 }
