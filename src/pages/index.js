066e1b755904eac9ac78e4c20820782baaf03778
malan88 <malan88@standingwater.io>
2019-12-12 11:04:56 -0500

    First panel, second panel image, no mobile optimization

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,25 @@ import styled from 'styled-components'
 import Img from 'gatsby-image'
 import BackgroundImage from 'gatsby-background-image'
 
+const HeroBlock = styled.div`
+    margin: 0 auto;
+    background: rgba(0, 0, 0, 0.6);
+    padding: 4rem;
+    color: white;
+    width: 50%;
+    h1 {
+        width: 60rem;
+        line-height: 1;
+        font-size: 5rem;
+        border-left: 1rem solid red;
+        padding: 0 0 0 1rem;
+    }
+    h2 {
+        font-size: 2.25rem;
+    }
+
+`
+
 class IndexPage extends React.Component {
     constructor(props) {
         super(props)
@@ -19,11 +38,21 @@ class IndexPage extends React.Component {
             <Layout>
                 <SEO title="Home" />
                 <Parallax pages={2}  ref={ref => (this.parallax = ref)}>
+
                     <ParallaxLayer offset={0} speed={0.25}>
-                        <Img fluid={this.props.data.file.childImageSharp.fluid} />
+                        <Img fluid={this.props.data.hero.childImageSharp.fluid} />
                     </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={0.25} style={{ height: '150vh', background: 'rgba(0, 0, 0, 0.4)' }} />
-                    <ParallaxLayer offset={1} speed={0.5} style={{ backgroundColor: 'darkred' }} />
+                    <ParallaxLayer offset={0.25} speed={0.4}>
+                        <HeroBlock>
+                            <h1>Serving the Southeast Since 1985</h1>
+                            <h2>With over three decades in the industry, Glendale has you covered.</h2>
+                        </HeroBlock>
+                    </ParallaxLayer>
+
+                    <ParallaxLayer offset={1} speed={0.5} >
+                        <Img fluid={this.props.data.mission.childImageSharp.fluid} />
+                    </ParallaxLayer>
+
                 </Parallax>
             </Layout>
         )
@@ -32,11 +61,26 @@ class IndexPage extends React.Component {
 
 export default IndexPage
 
+// fragment for simpmle fluid imports
+export const BGImg = graphql`
+    fragment BGImg on File {
+        childImageSharp {
+            fluid(quality: 100, maxWidth: 1920, duotone: { highlight: "#000000", shadow: "#000000", opacity: 40 }, toFormat: PNG) {
+                # the duotone is just a black screen for darkening bg images. To be adjusted when desired.
+                ...GatsbyImageSharpFluid
+            }
+        }
+    }
+`
+
 export const query = graphql`
     query {
-        file(relativePath: { eq: "jobs/BelleHarbor.jpg" }) {
+        hero: file(relativePath: { eq: "jobs/BelleHarbor.jpg" }) {
+            ...BGImg
+        }
+        mission: file(relativePath: { eq: "office-stock.jpg" }) {
             childImageSharp {
-                fluid(quality: 100, maxWidth: 1920) {
+                fluid(quality: 100, maxWidth: 1920, duotone: { highlight: "#f00e2e", shadow: "#000000" }, toFormat: PNG) {
                     ...GatsbyImageSharpFluid
                 }
             }
