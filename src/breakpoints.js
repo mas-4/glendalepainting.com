43f057520028cb0d51fb809b47eaded9bb5b064d
malan88 <malan88@standingwater.io>
2019-12-12 13:02:44 -0500

    Move dimensions, change breakpoints file

--- src/breakpoints.js
+++ src/breakpoints.js
@@ -1,7 +1,12 @@
-const bp = {
+export const nums = {
+    vp4:    480,
+    vp7:    768,
+    vp9:    992,
+    vp12:   1200,
+}
+export const bp = {
     vp4:  `(max-width: 480px)`,
     vp7:  `(max-width: 768px)`,
     vp9:  `(max-width: 992px)`,
     vp12: `(max-width: 1200px)`,
 }
-export default bp
--- src/dimensions.js
+++ src/dimensions.js
@@ -22,4 +22,3 @@ export default function useWindowDimensions() {
 
     return windowDimensions;
 }
-
