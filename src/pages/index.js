928b50e2b3bc0707597334fbb9383192cfdaf307
malan88 <malan88@standingwater.io>
2020-01-09 11:28:06 -0500

    Make progress toward a landing page
    - Hero Image & Text
    - Projects
    - Mission
    - Services
    - About Image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
-import React from "react"
+import React from 'react'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
-import { graphql } from "gatsby"
-import Img from "gatsby-image"
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
-import styled from "styled-components"
-import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons"
+import styled from 'styled-components'
+import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons'
+import ProjectsPanel from '../components/landing/projects'
 
 const HeroBlock = styled.div`
     margin: 0 auto;
@@ -41,6 +42,74 @@ const MissionText = styled.h1`
     margin: 0 0 0 45%;
 `
 
+const ServicesBox = () => {
+    const Title = styled.h1`
+        font-size: 500%;
+        width: 10rem;
+        margin: 0 auto 5rem auto;
+        padding: 0 0 0 1rem;
+        line-height: 0.9;
+        border-left 1rem solid red;
+        color: white;
+    `
+    const Boxes = styled.div`
+        display: flex;
+    `
+    const Box = styled.div`
+        width: 50%;
+        background: ${props => props.color};
+        display: flex;
+        flex-direction: column;
+        align-items: flex-${props => props.align};
+        padding: 5%;
+    `
+    const Text = styled.div`
+        width: 70%;
+        color: white;
+        h1 {
+            line-height: 0.8;
+        }
+    `
+    return (
+        <>
+            <Title>What We Do</Title>
+            <Boxes>
+                <Box
+                    color="rgba(41, 41, 41, 0.78)"
+                    align="end"
+                >
+                    <Text>
+                        <h1>Repaint &amp; Restoration</h1>
+                        <p>
+                            Whether it's the typical repaint, or a full-out
+                            restoration, our rehabilitation division has the
+                            knowledge and experience to get the job done.  Our
+                            employees know how to work with discerning clientele
+                            and make every resident happy with the end-product.
+                        </p>
+                    </Text>
+                </Box>
+                <Box
+                    color="rgba(241, 14, 14, 0.6)"
+                    align="start"
+                >
+                    <Text>
+                        <h1>New Construction Painting</h1>
+                        <p>
+                            Our new construction division boasts some of the
+                            largest and most complex structures in Florida.  We
+                            know how to meet the ever tightening deadlines
+                            developers and general contractors require these
+                            days.
+                        </p>
+                    </Text>
+                </Box>
+            </Boxes>
+        </>
+    )
+}
+
+
 class IndexPage extends React.Component {
     state = {
         width: window.innerWidth,
@@ -62,15 +131,23 @@ class IndexPage extends React.Component {
     render() {
         const heroImage = this.props.data.hero.childImageSharp.fluid;
         const aboutImage = this.props.data.about.childImageSharp.fluid;
+        const servicesImage = this.props.data.services.childImageSharp.fluid;
 
         return (
             <Layout>
                 <SEO title="Home" />
                 <Parallax
-                    pages={3}
+                    pages={10}
                     ref={ref => (this.parallax = ref)}
                     style={{ backgroundColor: "#FFFFFF" }}
                 >
+
+                    <ParallaxLayer offset={1} speed={0}>
+                        {/* this has to go above the hero panel so that it's underlying it */}
+                        <ProjectsPanel />
+                    </ParallaxLayer>
+
+
                     <ParallaxLayer
                         offset={0}
                         speed={0.5}
@@ -87,18 +164,27 @@ class IndexPage extends React.Component {
                         </HeroBlock>
                     </ParallaxLayer>
 
-                    <ParallaxLayer offset={2} speed={0.4}>
-                        <Img
-                            fluid={aboutImage}
-                        />
-                    </ParallaxLayer>
 
-
-                    <ParallaxLayer offset={1.5} speed={0.3}>
+                    <ParallaxLayer offset={3.9} speed={0.2}>
                         <MissionText>
                             Take care of the customer and everything else will take care of itself.
                         </MissionText>
                     </ParallaxLayer>
+
+
+                    <ParallaxLayer offset={4.5} speed={0}>
+                        <Img fluid={servicesImage} />
+                    </ParallaxLayer>
+                    <ParallaxLayer offset={4.97} speed={0}>
+                        <ServicesBox />
+                    </ParallaxLayer>
+
+
+                    <ParallaxLayer offset={5.62} speed={0}>
+                        <Img fluid={aboutImage} />
+                    </ParallaxLayer>
+
+
                 </Parallax>
             </Layout>
         )
@@ -145,6 +231,9 @@ export const query = graphql`
       }
       about: file(relativePath: { eq: "office-stock.jpg" }) {
           ...DuoToneImg
+      }
+      services: file(relativePath: { eq: "work/LaFirenzaDecksAdjusted.jpg" }) {
+          ...BGImg
       }
   }
 `
