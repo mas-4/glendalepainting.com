8f6925307c63a6de20b04a3ada4385e5e0a95a15
malan88 <malan88@standingwater.io>
2020-02-13 10:47:45 -0500

    Create Team and Testimonials pages

---
+++ src/pages/team.js
@@ -0,0 +1,11 @@
+import React from "react"
+import {Layout, SEO} from "../components/global"
+
+const TeamPage = () => (
+    <Layout>
+        <SEO title="Team" />
+        <h1>Team</h1>
+    </Layout>
+)
+
+export default TeamPage
---
+++ src/pages/testimonials.js
@@ -0,0 +1,11 @@
+import React from "react"
+import {Layout, SEO} from "../components/global"
+
+const TestimonialsPage = () => (
+    <Layout>
+        <SEO title="Testimonials" />
+        <h1>Testimonials</h1>
+    </Layout>
+)
+
+export default TestimonialsPage
