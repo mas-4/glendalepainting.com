a8b5ab3aa6032c94a232a3403d185d91f93fde01
malan88 <malan88@standingwater.io>
2020-02-21 11:36:41 -0500

    Fix scroll issue again

--- src/components/global/layout.js
+++ src/components/global/layout.js
@@ -13,11 +13,12 @@ const GlobalStyle = createGlobalStyle`
 const MainScroll = styled.main`
     display: grid;
     grid-template-rows: 9.6rem auto;
-    overflow-y: scroll;
     position: absolute;
     top: 0;
     left: 0;
     right: 0;
+    bottom: 0;
+    overflow-y: scroll;
 `;
 
 const MainNoScroll = styled.main`
