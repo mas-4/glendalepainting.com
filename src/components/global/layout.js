d4d6a6f981c071a3b96d59c578b04117a71e4cda
malan88 <malan88@standingwater.io>
2020-02-25 14:41:50 -0500

    Fix up footer

--- src/components/global/layout.js
+++ src/components/global/layout.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
 import styled, { createGlobalStyle } from 'styled-components';
 import * as reset from '../../styles/reset.css';
 import * as global from '../../styles/global.css';
 import { Header } from './header';
+import { Footer } from './footer';
 
 const GlobalStyle = createGlobalStyle`
     ${reset}
@@ -25,15 +25,6 @@ const MainNoScroll = styled.main`
     grid-template-rows: 9.6rem auto;
 `;
 
-const Footer = styled.footer`
-    height: 8rem;
-    background-color: ${({ theme }) => theme.lightGray};
-    margin-top: 4rem;
-    padding: 4rem;
-    text-align: center;
-    font-size: ${({ theme }) => theme.size2};
-`;
-
 export const LayoutScroll = ({ children }) => {
     return (
         <>
@@ -42,11 +33,7 @@ export const LayoutScroll = ({ children }) => {
             <MainScroll>
                 <div />
                 {children}
-                <Footer>
-                    © {new Date().getFullYear()} Glendale Painting Corporation,
-                    {` `}
-                    Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
-                </Footer>
+                <Footer />
             </MainScroll>
         </>
     );
@@ -60,11 +47,7 @@ export const Layout = ({ children }) => {
             <MainNoScroll>
                 <div />
                 {children}
-                <Footer>
-                    © {new Date().getFullYear()} Glendale Painting Corporation,
-                    {` `}
-                    Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
-                </Footer>
+                <Footer />
             </MainNoScroll>
         </>
     );
