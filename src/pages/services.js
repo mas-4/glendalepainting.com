b4deb6b1111b612e7af32448676d11fdb725b816
malan88 <malan88@standingwater.io>
2020-02-27 13:31:32 -0500

    Fix up services page

--- src/pages/services.js
+++ src/pages/services.js
@@ -10,32 +10,32 @@ const Container = styled.div`
     justify-content: center;
 `
 const Item = styled.div`
-    width: 20%;
+    width: 38rem;
     background-color: ${({theme}) => theme.lightGray};
     margin: 0.8rem;
-    padding: 0 2%;
+    padding: 0 3rem;
     border-radius: 1.6rem;
     border: 2px solid #d7d7d7;
 
     img {
         width: 100%;
-        margin: 3.2rem auto 0;
+        margin: 2rem auto 0 auto;
         border-radius: 0.64rem;
         border: 1px solid #d7d7d7;
     }
     h1 {
-        font-size: 1.8rem;
-        color: red;
+        font-size: ${({theme}) => theme.size3};
+        color: ${({theme}) => theme.red};
         margin: 11.8px 0;
     }
 
     h2 {
-        font-size: 1.6rem;
+        font-size: ${({theme}) => theme.size2};
         margin-top: 13.2px;
     }
 
     p {
-        font-size: 1.6rem;
+        font-size: ${({theme}) => theme.size2};
         margin: 2rem 0;
     }
 `
@@ -43,7 +43,7 @@ const Item = styled.div`
 const Panel = ({info}) => {
     return (
         <Item>
-            <img src={info.pic} style={{ width: '300px', margin: '2rem auto 0 auto' }} alt={info.h1} />
+            <img src={info.pic}  alt={info.h1} />
             <h1>{info.h1}</h1>
             <h2>{info.h2}</h2>
             {info.body.map(para => <p>{para}</p>)}
