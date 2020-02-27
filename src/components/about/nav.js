ad68d3635ac13a306397d0c2de2daa173fe52b7d
malan88 <malan88@standingwater.io>
2020-02-27 12:29:39 -0500

    Fix nav bar styling

--- src/components/about/nav.js
+++ src/components/about/nav.js
@@ -4,23 +4,28 @@ import styled from 'styled-components';
 
 const NavBar = styled.nav`
     width: fit-content;
-    background-color: rgb(255, 255, 255, 0.65);
     margin: 0 auto;
     display: flex;
     justify-content: space-between;
 `;
 const NavLink = styled(Link)`
-    color: ${({ theme }) => theme.black};
-    font-size: ${({ theme }) => theme.size4};
+    color: ${({ theme }) => theme.white};
+    font-size: ${({ theme }) => theme.size2};
+    font-weight: 700;
     text-decoration: none;
     text-transform: uppercase;
-    padding: 0 1.6rem;
+    padding: 1rem 1.6rem;
+    margin: 1rem;
     transition: 0.33s;
+    background-color: ${({theme}) => theme.lightGray};
 
     &:hover,
     &.active {
         background-color: ${({ theme }) => theme.red};
         color: ${({ theme }) => theme.white};
+    }
+    &:hover {
+        box-shadow: ${({ theme }) => theme.boxShadow};
     }
 `;
 
