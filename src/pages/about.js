76125c3833814ca3fc8688016b794ef3ce5297cf
malan88 <malan88@standingwater.io>
2020-04-02 13:30:15 -0400

    Create framework for about-mobile-opt

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ import styled from 'styled-components'
 import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons'
 import { AboutNav } from '../components/about/nav'
 import { Layout, SEO } from '../components/global'
+import { breakpoints, numbers} from '../styles/breakpoints'
 
 const HeroTitle = styled.h1`
     color: ${({theme}) => theme.white};
@@ -56,155 +57,181 @@ const SeeMore = styled.div`
         text-decoration: none;
     }
 `
+const SmallAbout = () => {
+    return (
+        <Layout />
+    )
+}
+const BigAbout = ({ heroImage, testimonialsImage, width }) => {
+    return (
+        <Layout>
+            <SEO title="About" />
+            <Parallax
+                pages={2.25}
+                style={{ backgroundColor: "#FFFFFF" }}
+            >
+                <ParallaxLayer offset={0} speed={0}>
+                    <Img fluid={heroImage} />
+                </ParallaxLayer>
+                <ParallaxLayer offset={0.125} speed={0}>
+                    <AboutNav />
+                </ParallaxLayer>
+                <ParallaxLayer offset={1} speed={0}>
+                    <Img fluid={testimonialsImage} />
+                </ParallaxLayer>
+                <ParallaxLayer offset={0.25} speed={0.25}>
+                    <HeroTitle>Who We Are</HeroTitle>
+                </ParallaxLayer>
+                <ParallaxLayer offset={0.50} speed={0.45}>
+                    <AboutBox />
+                </ParallaxLayer>
+                <ParallaxLayer offset={0.50} speed={0.45}>
+                    <AboutParagraph left={true}>
+                        Founded in 1985, Glendale is one of the most successful
+                        commercial painting contractors in the Southeast. Quality and
+                        reliability has led to our Success.  Our company name may not be
+                        widely recognized, but we're well-known in the industry.  We
+                        don't put street signs out on every project, we don't advertise
+                        in the trade or community publications, and we don't attend all
+                        the trade functions displaying our signs and marketing
+                        giveaways.  We expand by word-of-mouth alone.  We expand by
+                        doing good work for a competitive price, by doing it right the
+                        first time so there's no need for call-backs or warranty claims,
+                        by servicing our clients above and beyond their expectations.
+                    </AboutParagraph>
+                    <AboutParagraph left={false}>
+                        The Glendale Painting Team has successfully completed projects
+                        as small as $5,000 and as large as $3,500,000.  Our project
+                        portfolio is extensive and contains a balanced blend of New
+                        Construction Projects with Repaint/Restoration Projects.  The
+                        company is fully bonded and insured and can provide payment and
+                        performance bonds on request.
+                    </AboutParagraph>
+                </ParallaxLayer>
+                <ParallaxLayer offset={0.99} speed={0.5}>
+                    <AboutParagraph left={true}>
+                        Unlike most of the competition, our company directly employs all
+                        its workers.  When you hire Glendale, you get Glendale. Unlike
+                        most of the competition, Glendale is not a broker subletting the
+                        work. All of our work is performed in-house.  We understand the
+                        importance of quality workmanship to the customer, our
+                        reputation, and our future.  This "old school approach" provides
+                        more control on the project and insures specifications are
+                        followed with quality as the main goal.
+                    </AboutParagraph>
+                    <AboutParagraph left={false}>
+                        One of the highest compliments a company like ours can receive
+                        is when a long time employee decides it's time to spread their
+                        wings and start their own company. We take pride in knowing
+                        these new entrepreneurs have learned it the right way.
+                    </AboutParagraph>
+                </ParallaxLayer>
+                <ParallaxLayer offset={1.25} speed={0.4}>
+                    <HeroTitle>What People Say</HeroTitle>
+                </ParallaxLayer>
+                <ParallaxLayer offset={1.5} speed={0.5}>
+                    <Testimonial>
+                        <p>
+                            As Glendale nears the final stage of completing
+                            touch-ups and wrapping up remaining punch list items
+                            here at Belle Harbor, I wanted to extend to you and
+                            your entire staff a congratulatory 'Thank You', for
+                            a big job that was well done, and which remained on
+                            schedule. With 200 individual owners here, which
+                            often can translate into 200 individual “bosses”,
+                            the hands-on, customer responsiveness skills
+                            practiced by you and your employees were quite
+                            evident and always on display. In particular, having
+                            Kevin as an on-site supervisor coupled with your
+                            extremely capable crew foreman “Jared”, and with
+                            each of them working hand in hand with one another,
+                            their jointly coordinated efforts have contributed
+                            to a well scheduled smooth and effectively
+                            orchestrated project for us at Belle Harbor.
+                        </p>
+                        <p>
+                            Don Mestas, Jr. - President - Belle Harbor Owner's Association
+                        </p>
+                    </Testimonial>
+                </ParallaxLayer>
+                <ParallaxLayer offset={1.75} speed={0.6}>
+                    <Testimonial>
+                        <p>
+                            We can’t say enough to convey how pleased we
+                            were with Kevin’s ability, as well as his
+                            personality, in overseeing the crew supervisor
+                            and entire project. We were equally pleased with
+                            Jared, the crew supervisor who managed the crew
+                            daily.
+                        </p>
 
+                        <p>
+                            "Additionally, I’d like to convey our Board
+                            received many compliments from our residents
+                            regarding the selection of Glendale Painting.
+                            They commented on how the crew supervisor and
+                            painters were courteous and interacted
+                            pleasantly with our residents. The overall
+                            experience and expectations were not only met,
+                            but exceeded.
+                        </p>
+
+                        <p>
+                            Henri Tackett – President – Bermuda Board of Directors
+                        </p>
+                    </Testimonial>
+                </ParallaxLayer>
+                <ParallaxLayer offset={1.99} speed={0.7}>
+                    <Testimonial>
+                        <p>
+                            Thanks for everything Rick.  The project has
+                            turned out great and I appreciate your
+                            professionalism.  Kevin was experienced,
+                            efficient and motivated.  He provided constant
+                            on-site supervision and immediate solutions.
+                            Rick, you were always willing to accommodate
+                            requests.
+                        </p>
+                        <p>
+                            Gail Chase - Chief Operating Officer - Freedom Senior Management
+                        </p>
+                    </Testimonial>
+                </ParallaxLayer>
+                <ParallaxLayer offset={2} speed={0.07}>
+                    <SeeMore>
+                        <Link to='/testimonials'>
+                            See more testimonials
+                        </Link>
+                    </SeeMore>
+                </ParallaxLayer>
+            </Parallax>
+        </Layout>
+    )
+}
 class AboutPage extends React.Component {
+    state = {
+        width: window.innerWidth,
+        height: window.innerHeight
+    }
+    componentDidMount() {
+        window.addEventListener('resize', this.handleResize)
+    }
+    componentWillUnmount(){
+        window.removeEventListener('resize', this.handleResize)
+    }
+    handleResize = () => {
+        this.setState({width: window.innerWidth, height: window.innerHeight})
+    }
     render() {
-        const heroImage = this.props.data.hero.childImageSharp.fluid;
-        const testimonialsImage = this.props.data.testimonials.childImageSharp.fluid;
-        return (
-            <Layout>
-                <SEO title="About" />
-                <Parallax
-                    pages={2.25}
-                    ref={ref => (this.parallax = ref)}
-                    style={{ backgroundColor: "#FFFFFF" }}
-                >
-                    <ParallaxLayer offset={0} speed={0}>
-                        <Img fluid={heroImage} />
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={0.125} speed={0}>
-                        <AboutNav />
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={1} speed={0}>
-                        <Img fluid={testimonialsImage} />
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={0.25} speed={0.25}>
-                        <HeroTitle>Who We Are</HeroTitle>
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={0.50} speed={0.45}>
-                        <AboutBox />
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={0.50} speed={0.45}>
-                        <AboutParagraph left={true}>
-                            Founded in 1985, Glendale is one of the most successful
-                            commercial painting contractors in the Southeast. Quality and
-                            reliability has led to our Success.  Our company name may not be
-                            widely recognized, but we're well-known in the industry.  We
-                            don't put street signs out on every project, we don't advertise
-                            in the trade or community publications, and we don't attend all
-                            the trade functions displaying our signs and marketing
-                            giveaways.  We expand by word-of-mouth alone.  We expand by
-                            doing good work for a competitive price, by doing it right the
-                            first time so there's no need for call-backs or warranty claims,
-                            by servicing our clients above and beyond their expectations.
-                        </AboutParagraph>
-                        <AboutParagraph left={false}>
-                            The Glendale Painting Team has successfully completed projects
-                            as small as $5,000 and as large as $3,500,000.  Our project
-                            portfolio is extensive and contains a balanced blend of New
-                            Construction Projects with Repaint/Restoration Projects.  The
-                            company is fully bonded and insured and can provide payment and
-                            performance bonds on request.
-                        </AboutParagraph>
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={0.99} speed={0.5}>
-                        <AboutParagraph left={true}>
-                            Unlike most of the competition, our company directly employs all
-                            its workers.  When you hire Glendale, you get Glendale. Unlike
-                            most of the competition, Glendale is not a broker subletting the
-                            work. All of our work is performed in-house.  We understand the
-                            importance of quality workmanship to the customer, our
-                            reputation, and our future.  This "old school approach" provides
-                            more control on the project and insures specifications are
-                            followed with quality as the main goal.
-                        </AboutParagraph>
-                        <AboutParagraph left={false}>
-                            One of the highest compliments a company like ours can receive
-                            is when a long time employee decides it's time to spread their
-                            wings and start their own company. We take pride in knowing
-                            these new entrepreneurs have learned it the right way.
-                        </AboutParagraph>
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={1.25} speed={0.4}>
-                        <HeroTitle>What People Say</HeroTitle>
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={1.5} speed={0.5}>
-                        <Testimonial>
-                            <p>
-                                As Glendale nears the final stage of completing
-                                touch-ups and wrapping up remaining punch list items
-                                here at Belle Harbor, I wanted to extend to you and
-                                your entire staff a congratulatory 'Thank You', for
-                                a big job that was well done, and which remained on
-                                schedule. With 200 individual owners here, which
-                                often can translate into 200 individual “bosses”,
-                                the hands-on, customer responsiveness skills
-                                practiced by you and your employees were quite
-                                evident and always on display. In particular, having
-                                Kevin as an on-site supervisor coupled with your
-                                extremely capable crew foreman “Jared”, and with
-                                each of them working hand in hand with one another,
-                                their jointly coordinated efforts have contributed
-                                to a well scheduled smooth and effectively
-                                orchestrated project for us at Belle Harbor.
-                            </p>
-                            <p>
-                                Don Mestas, Jr. - President - Belle Harbor Owner's Association
-                            </p>
-                        </Testimonial>
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={1.75} speed={0.6}>
-                        <Testimonial>
-                            <p>
-                                We can’t say enough to convey how pleased we
-                                were with Kevin’s ability, as well as his
-                                personality, in overseeing the crew supervisor
-                                and entire project. We were equally pleased with
-                                Jared, the crew supervisor who managed the crew
-                                daily.
-                            </p>
+        const About = this.state.width > numbers.vp9 ? BigAbout : SmallAbout;
+        return(
+            <About
+                width={this.state.width}
+                height={this.state.height}
+                heroImage={this.props.data.hero.childImageSharp.fluid}
+                testimonialsImage={this.props.data.testimonials.childImageSharp.fluid}
+            />
 
-                            <p>
-                                "Additionally, I’d like to convey our Board
-                                received many compliments from our residents
-                                regarding the selection of Glendale Painting.
-                                They commented on how the crew supervisor and
-                                painters were courteous and interacted
-                                pleasantly with our residents. The overall
-                                experience and expectations were not only met,
-                                but exceeded.
-                            </p>
-
-                            <p>
-                                Henri Tackett – President – Bermuda Board of Directors
-                            </p>
-                        </Testimonial>
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={1.99} speed={0.7}>
-                        <Testimonial>
-                            <p>
-                                Thanks for everything Rick.  The project has
-                                turned out great and I appreciate your
-                                professionalism.  Kevin was experienced,
-                                efficient and motivated.  He provided constant
-                                on-site supervision and immediate solutions.
-                                Rick, you were always willing to accommodate
-                                requests.
-                            </p>
-                            <p>
-                                Gail Chase - Chief Operating Officer - Freedom Senior Management
-                            </p>
-                        </Testimonial>
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={2} speed={0.07}>
-                        <SeeMore>
-                            <Link to='/testimonials'>
-                                See more testimonials
-                            </Link>
-                        </SeeMore>
-                    </ParallaxLayer>
-                </Parallax>
-            </Layout>
         )
     }
 }
