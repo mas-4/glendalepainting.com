99daa7fa2ab325e6885727eff2e31a909a1eb0c9
malan88 <malan88@standingwater.io>
2020-04-24 09:18:47 -0400

    Optimize about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ import styled from 'styled-components'
 import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons'
 
 import { AboutNav } from '../components/about/nav'
-import { Layout, SEO } from '../components/global'
+import { Layout, SEO, LayoutScroll } from '../components/global'
 import { breakpoints, numbers} from '../styles/breakpoints'
 
 const HeroTitle = styled.h1`
@@ -20,6 +20,9 @@ const HeroTitle = styled.h1`
     ${breakpoints.vp13} {
         font-size: 6.5rem;
     }
+    ${breakpoints.vp6} {
+        width: 45%;
+    }
 `
 const AboutBox = styled.div`
     background: rgba(0, 0, 0, 0.2);
@@ -28,6 +31,18 @@ const AboutBox = styled.div`
     height: 96rem;
     ${breakpoints.vp13} {
         height: 78rem;
+    }
+    ${breakpoints.vp9} {
+        width: 85%;
+        margin-top: 2rem;
+        padding: 1rem;
+        height: auto;
+    }
+    ${breakpoints.vp6} {
+        width: 95%;
+    }
+    ${breakpoints.vp4} {
+        width: 100%;
     }
 `
 const AboutParagraph = styled.div`
@@ -42,6 +57,13 @@ const AboutParagraph = styled.div`
         margin-left: ${props => props.left ? '28' : '36'}rem;
         margin-top: 3rem;
         font-size: 1.75rem;
+    }
+    ${breakpoints.vp9} {
+        width: 85%;
+        margin-left: ${props => props.left ? '5' : '10'}rem;
+    }
+    ${breakpoints.vp6} {
+        margin-left: ${props => props.left ? '2' : '6'}rem;
     }
 `
 const Testimonial = styled.div`
@@ -59,6 +81,18 @@ const Testimonial = styled.div`
         color: red;
         font-weight: bold;
     }
+    ${breakpoints.vp9} {
+        margin-top: 2rem;
+    }
+    ${breakpoints.vp7} {
+        width: 80%;
+    }
+    ${breakpoints.vp6} {
+        width: 95%;
+    }
+    ${breakpoints.vp4} {
+        width: 100%;
+    }
 `
 const SeeMore = styled.div`
     background-color: red;
@@ -72,9 +106,152 @@ const SeeMore = styled.div`
         text-decoration: none;
     }
 `
-const SmallAbout = () => {
+const Background = ({ image, className, children }) => {
     return (
-        <Layout />
+        <BackgroundImage
+            Tag="section"
+            backgroundColor={`#040e18`}
+            fluid={image}
+            className={className}
+        >
+            {children}
+        </BackgroundImage>
+    )
+}
+const StyledBackground = styled(Background)`
+    /* Create the parallax scrolling effect */
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+`
+const Spacer = styled.div`
+    height: 1.5rem;
+    ${breakpoints.vp4_2} {
+        height: 5rem;
+    }
+`
+const SmallAbout = ({ heroImage, testimonialsImage, width }) => {
+    return (
+        <LayoutScroll>
+            <SEO title="Home" />
+            <StyledBackground image={heroImage}>
+                <Spacer />
+                <HeroTitle>Who We Are</HeroTitle>
+                <AboutBox>
+                    <AboutParagraph left={true}>
+                        Founded in 1985, Glendale is one of the most successful
+                        commercial painting contractors in the Southeast. Quality and
+                        reliability has led to our Success.  Our company name may not be
+                        widely recognized, but we're well-known in the industry.  We
+                        don't put street signs out on every project, we don't advertise
+                        in the trade or community publications, and we don't attend all
+                        the trade functions displaying our signs and marketing
+                        giveaways.  We expand by word-of-mouth alone.  We expand by
+                        doing good work for a competitive price, by doing it right the
+                        first time so there's no need for call-backs or warranty claims,
+                        by servicing our clients above and beyond their expectations.
+                    </AboutParagraph>
+                    <AboutParagraph left={false}>
+                        The Glendale Painting Team has successfully completed projects
+                        as small as $5,000 and as large as $3,500,000.  Our project
+                        portfolio is extensive and contains a balanced blend of New
+                        Construction Projects with Repaint/Restoration Projects.  The
+                        company is fully bonded and insured and can provide payment and
+                        performance bonds on request.
+                    </AboutParagraph>
+                    <AboutParagraph left={true}>
+                        Unlike most of the competition, our company directly employs all
+                        its workers.  When you hire Glendale, you get Glendale. Unlike
+                        most of the competition, Glendale is not a broker subletting the
+                        work. All of our work is performed in-house.  We understand the
+                        importance of quality workmanship to the customer, our
+                        reputation, and our future.  This "old school approach" provides
+                        more control on the project and insures specifications are
+                        followed with quality as the main goal.
+                    </AboutParagraph>
+                    <AboutParagraph left={false}>
+                        One of the highest compliments a company like ours can receive
+                        is when a long time employee decides it's time to spread their
+                        wings and start their own company. We take pride in knowing
+                        these new entrepreneurs have learned it the right way.
+                    </AboutParagraph>
+                </AboutBox>
+                <Spacer />
+            </StyledBackground>
+            <StyledBackground image={testimonialsImage}>
+                <Spacer />
+                <HeroTitle>What People Say</HeroTitle>
+                <Testimonial>
+                    <p>
+                        As Glendale nears the final stage of completing
+                        touch-ups and wrapping up remaining punch list items
+                        here at Belle Harbor, I wanted to extend to you and
+                        your entire staff a congratulatory 'Thank You', for
+                        a big job that was well done, and which remained on
+                        schedule. With 200 individual owners here, which
+                        often can translate into 200 individual “bosses”,
+                        the hands-on, customer responsiveness skills
+                        practiced by you and your employees were quite
+                        evident and always on display. In particular, having
+                        Kevin as an on-site supervisor coupled with your
+                        extremely capable crew foreman “Jared”, and with
+                        each of them working hand in hand with one another,
+                        their jointly coordinated efforts have contributed
+                        to a well scheduled smooth and effectively
+                        orchestrated project for us at Belle Harbor.
+                    </p>
+                    <p>
+                        Don Mestas, Jr. - President - Belle Harbor Owner's Association
+                    </p>
+                </Testimonial>
+                <Testimonial>
+                    <p>
+                        We can’t say enough to convey how pleased we
+                        were with Kevin’s ability, as well as his
+                        personality, in overseeing the crew supervisor
+                        and entire project. We were equally pleased with
+                        Jared, the crew supervisor who managed the crew
+                        daily.
+                    </p>
+
+                    <p>
+                        "Additionally, I’d like to convey our Board
+                        received many compliments from our residents
+                        regarding the selection of Glendale Painting.
+                        They commented on how the crew supervisor and
+                        painters were courteous and interacted
+                        pleasantly with our residents. The overall
+                        experience and expectations were not only met,
+                        but exceeded.
+                    </p>
+
+                    <p>
+                        Henri Tackett – President – Bermuda Board of Directors
+                    </p>
+                </Testimonial>
+                <Testimonial>
+                    <p>
+                        Thanks for everything Rick.  The project has
+                        turned out great and I appreciate your
+                        professionalism.  Kevin was experienced,
+                        efficient and motivated.  He provided constant
+                        on-site supervision and immediate solutions.
+                        Rick, you were always willing to accommodate
+                        requests.
+                    </p>
+                    <p>
+                        Gail Chase - Chief Operating Officer - Freedom Senior Management
+                    </p>
+                </Testimonial>
+                <SeeMore style={{ marginTop: '1rem' }}>
+                    <Link to='/testimonials'>
+                        See more testimonials
+                    </Link>
+                </SeeMore>
+                <Spacer />
+            </StyledBackground>
+        </LayoutScroll>
     )
 }
 const BigAbout = ({ heroImage, testimonialsImage, width }) => {
@@ -241,6 +418,7 @@ const BigAbout = ({ heroImage, testimonialsImage, width }) => {
         </Layout>
     )
 }
+
 class AboutPage extends React.Component {
     state = {
         width: window.innerWidth,
@@ -268,7 +446,6 @@ class AboutPage extends React.Component {
         )
     }
 }
-
 export default AboutPage
 
 export const BGImgAbout = graphql`
